2df49b02f2bcade544796d2bf943183d057418a2
Masata NISHIDA <masatanish@gmail.com>
2012-04-04 21:53:08 +0900

    modified setup script

--- setup.sh
+++ setup.sh
@@ -7,7 +7,11 @@ DOT_FILES=( .vim .vimrc )
 for file in ${DOT_FILES[@]}
 do
     if [ -a $HOME/$file ]; then
-        echo "file already exists: $file"
+        if [ -L $HOME/$file ]; then
+            echo "symbolic link exists: $file"
+        else
+            echo "WARNING: file already exists: $file"
+        fi
     else
         ln -s $/HOME/dotfiles/$file $HOME/$file
         echo "created symbolic link: $file"
