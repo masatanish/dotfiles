8f6ebf00d0069540feb4bb05d7f7eeaa80c33787
masatanish <masatanish@gmail.com>
2013-11-15 15:25:42 +0900

    add .gitignore_global to dotfiles

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 
 echo "set up dotfiles..."
 echo "create symbolic links..."
-DOT_FILES=( .vimrc .zshrc .vrapperrc .rspec .tmux.conf )
+DOT_FILES=( .vimrc .zshrc .vrapperrc .rspec .tmux.conf .gitignore_global)
 
 for file in ${DOT_FILES[@]}
 do
